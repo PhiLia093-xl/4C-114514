733449df626ff0d12eeabd1c779a17d32f29a1ec
PhiLia093-xl <2793107795@qq.com>
2026-03-24 19:29:48 +0800

    书本添加内容

--- Assets/Script/BookManager.cs
+++ Assets/Script/BookManager.cs
@@ -5,48 +5,47 @@ using UnityEngine.SceneManagement;
 public class BookManager : MonoBehaviour
 {
     public Button[] books = new Button[8];
-
-    bool[] unlocked = new bool[8];
-    bool[] finished = new bool[8];
+    private bool[] unlocked = new bool[8];
+    private bool[] finished = new bool[8];
 
     void Start()
     {
+        RefreshAll();
+    }
+
+    // 封装刷新逻辑，方便初始化和重置时调用
+    void RefreshAll()
+    {
         LoadData();
-        UpdateUnlock();
+        UpdateUnlockStatus();
         RefreshUI();
     }
 
     void LoadData()
     {
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < books.Length; i++)
         {
+            // 读取每本书的完成状态 (0: 未完成, 1: 已完成)
             finished[i] = PlayerPrefs.GetInt("BookFinished_" + i, 0) == 1;
         }
     }
 
-    void UpdateUnlock()
+    void UpdateUnlockStatus()
     {
+        // 第一本书默认永远解锁
         unlocked[0] = true;
 
-        if (finished[0])
-            unlocked[1] = true;
-
-        if (finished[1])
+        // 从第二本书开始循环：如果前一本书完成了，则当前书解锁
+        for (int i = 1; i < books.Length; i++)
         {
-            unlocked[2] = true;
-            unlocked[3] = true;
-        }
-
-        if (finished[2] && finished[3])
-        {
-            unlocked[4] = true;
-            unlocked[5] = true;
-        }
-
-        if (finished[4] && finished[5])
-        {
-            unlocked[6] = true;
-            unlocked[7] = true;
+            if (finished[i - 1])
+            {
+                unlocked[i] = true;
+            }
+            else
+            {
+                unlocked[i] = false;
+            }
         }
     }
 
@@ -54,20 +53,45 @@ public class BookManager : MonoBehaviour
     {
         for (int i = 0; i < books.Length; i++)
         {
+            // 设置按钮是否可点击
             books[i].interactable = unlocked[i];
+
+            // 进阶：你可以根据解锁状态改变颜色或透明度
+            Image img = books[i].GetComponent<Image>();
+            if (img != null)
+            {
+                img.color = unlocked[i] ? Color.white : Color.gray;
+            }
         }
     }
 
     public void OpenBook(int index)
     {
-        if (!unlocked[index])
+        if (index < 0 || index >= books.Length || !unlocked[index])
         {
-            Debug.Log("这本书还没解锁");
+            Debug.Log("这本书还没解锁或索引错误");
             return;
         }
 
         PlayerPrefs.SetInt("ReadingBook", index);
-
         SceneManager.LoadScene("ReadScene");
+    }
+
+    // --- 新增功能：重置按钮调用此方法 ---
+    public void ResetProgress()
+    {
+        // 删除所有相关的 PlayerPrefs 键
+        for (int i = 0; i < books.Length; i++)
+        {
+            PlayerPrefs.DeleteKey("BookFinished_" + i);
+        }
+
+        PlayerPrefs.DeleteKey("ReadingBook");
+        PlayerPrefs.Save(); // 强制保存更改
+
+        Debug.Log("进度已重置");
+
+        // 重新刷新数据和UI
+        RefreshAll();
     }
 }
